bf192d59c58053451d2f5253016a11d3abeab7d4
lin.guo <lin.guo@jiangrongxin.com>
2019-03-12 14:50:23 +0800

    添加主函数后提交

---
+++ src/com/jrx/Demo.java
@@ -0,0 +1,13 @@
+package com.jrx;
+
+/**
+ * 测试
+ *
+ * @author
+ * @create 2019-03-12 14:49
+ **/
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("git测试");
+    }
+}
